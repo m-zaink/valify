7d9a965a8dfb0090e6e372d977f48166b0649d8f
mzaink <mzainkofficial@gmail.com>
2020-11-10 19:00:26 +0530

    Refactor into better folder structure

--- lib/valify.dart
+++ lib/valify.dart
@@ -1,4 +1,5 @@
 library valify;
 
-export 'package:valify/src/constraints/input_constraint.dart';
 export 'package:valify/src/valifier/valifier.dart';
+export 'package:valify/src/constraints/input_constraint.dart';
+export 'package:valify/src/constraints/primary_constraints/primary_constraints.dart';
